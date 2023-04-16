edd9e464c204a1ae870ec01103a5083425e8fc76
guoluojie <kuo9roger@gmail.com>
2023-04-16 14:56:16 -0700

    custom detect image

--- detection_custom.py
+++ detection_custom.py
@@ -9,19 +9,30 @@
 #
 #================================================================
 import os
+import sys
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 import cv2
 import numpy as np
 import tensorflow as tf
 from yolov3.utils import detect_image, detect_realtime, detect_video, Load_Yolo_model, detect_video_realtime_mp
 from yolov3.configs import *
+from yolov3.yolov4 import Create_Yolo
 
-image_path   = "./IMAGES/plate_2.jpg"
-video_path   = "./IMAGES/test.mp4"
+# Change input image
+image_path = "./FlowChart/FlowChart_test/writer000_fc_011.png"
+if len(sys.argv) > 1:
+    image_path = sys.argv[1]
+    if not os.path.exists(image_path) or image_path is None:
+        print("Image path not valid, using default")
+        image_path = "./FlowChart/FlowChart_test/writer000_fc_011.png"
+else:
+    print("No given image path, using default")
+    
+# image_path = input("Enter image path: ")
+# if not os.path.exists(image_path):
+#     print("Path doesn't exist. Using default")
+#     image_path = "./FlowChart/FlowChart_test/writer000_fc_011.png"
 
-yolo = Load_Yolo_model()
-detect_image(yolo, image_path, "./IMAGES/plate_1_detect.jpg", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
-#detect_video(yolo, video_path, './IMAGES/detected.mp4', input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
-#detect_realtime(yolo, '', input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255, 0, 0))
-
-#detect_video_realtime_mp(video_path, "Output.mp4", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0), realtime=False)
+yolo = Create_Yolo(input_size=YOLO_INPUT_SIZE, CLASSES=TRAIN_CLASSES)
+yolo.load_weights("./checkpoints/yolov4_custom_Tiny")
+detect_image(yolo, image_path, "", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
